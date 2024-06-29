0036712a9d2ca2bdf5607c77e47b06c6536b8326
Promise <85255822+Bafana-Promise@users.noreply.github.com>
2024-06-29 15:06:17 +0200

    Added a flex on expense elements and added Common Module and Router Module

--- src/app/pages/expense/expense.component.ts
+++ src/app/pages/expense/expense.component.ts
@@ -1,9 +1,11 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-expense',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, RouterModule],
   templateUrl: './expense.component.html',
   styleUrl: './expense.component.scss'
 })
