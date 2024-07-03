e3333eee77813e0cbba96a8a35f258343fdc7912
Promise <85255822+Bafana-Promise@users.noreply.github.com>
2024-07-03 11:54:28 +0200

    Added errors on expense html and condition with dependies on TS, installed angular/fire,  on constants.ts added firebaseConfig and app.config.ts importProvidersFrom

---
+++ src/app/core/constants/constants.ts
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDZQt-fhjHjeALXx6CTz4sf9RcfEwzXFBU",
+  authDomain: "ng17-firebase-crud-app-88df4.firebaseapp.com",
+  databaseURL: "https://ng17-firebase-crud-app-88df4-default-rtdb.firebaseio.com",
+  projectId: "ng17-firebase-crud-app-88df4",
+  storageBucket: "ng17-firebase-crud-app-88df4.appspot.com",
+  messagingSenderId: "281914663992",
+  appId: "1:281914663992:web:bce331b27609b1799272cd",
+  measurementId: "G-NQ30TJ1N43"
+};
+
+// Initialize Firebase
--- src/app/pages/expense-form/expense-form.component.ts
+++ src/app/pages/expense-form/expense-form.component.ts
@@ -1,12 +1,45 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-expense-form',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, RouterModule, ReactiveFormsModule],
   templateUrl: './expense-form.component.html',
   styleUrl: './expense-form.component.scss'
 })
-export class ExpenseFormComponent {
+export class ExpenseFormComponent implements OnInit{
+  expenseForm!: FormGroup;
+
+  constructor(private fb: FormBuilder){
+    this.expenseForm = this.fb.group({
+      price: new FormControl('', [Validators.required]),
+      title: new FormControl('', [Validators.required]),
+      description: new FormControl(''),
+    })
+  }
+
+  ngOnInit(): void {
+
+  }
+
+  onSubmit(){
+    if(this.expenseForm.valid){
+      console.log('Form ==>', this.expenseForm.value);
+    }else{
+      this.expenseForm.markAllAsTouched();
+    }
+  }
+
+  // onSubmit(form: any){
+  //   if(this.expenseForm.valid){
+  //     console.log(this.expenseForm.value);
+  //     console.log("form ===>", form)
+  //   }else{
+  //     this.expenseForm.markAllAsTouched();
+  //   }
+  // }
 
 }
