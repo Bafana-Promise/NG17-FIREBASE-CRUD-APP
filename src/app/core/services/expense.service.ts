3cf9abd6863671b5c1771c8e884c5decdb10f2bd
Promise <85255822+Bafana-Promise@users.noreply.github.com>
2024-07-03 13:08:35 +0200

    Added a Crup on expense service

--- src/app/core/services/expense.service.ts
+++ src/app/core/services/expense.service.ts
@@ -1,9 +1,38 @@
 import { Injectable } from '@angular/core';
+import { AngularFireDatabase, AngularFireList } from '@angular/fire/compat/database';
+import { IExpense } from '../models/common.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ExpenseService {
 
-  constructor() { }
+  private dbPath = '/expense'
+  expensesRef: AngularFireList<any>
+  key: any;
+
+  constructor(private db: AngularFireDatabase) {
+    this.expensesRef = db.list(this.dbPath);
+  }
+
+  getAllExpenses(){
+    return this.expensesRef;
+  }
+  // Get Expense by Id so that we can Edit
+  getExpense(key: string){
+    return this.db.object(`${this.dbPath}/${key}`)
+  }
+
+  addExpense(expense: IExpense){
+    this.expensesRef.push(expense);
+  }
+
+  updateExpense(key: string, expense: IExpense){
+    this.expensesRef.update(key, expense);
+  }
+
+  deleteExpense(key: string){
+    return this.expensesRef.remove(key);
+  }
+
 }
