b65bfb207f76eead6d62a6428034d55fe2b60d09
Promise <85255822+Bafana-Promise@users.noreply.github.com>
2024-07-04 09:33:43 +0200

    Mapping data accordingly and displaying to UI

--- src/app/pages/expense/expense.component.ts
+++ src/app/pages/expense/expense.component.ts
@@ -11,33 +11,40 @@ import { IExpense } from '../../core/models/common.model';
   templateUrl: './expense.component.html',
   styleUrl: './expense.component.scss'
 })
-export class ExpenseComponent implements OnInit{
+export class ExpenseComponent implements OnInit {
 
   expenses: IExpense[] = [];
   totalExpenses = 0;
 
-  constructor(private expenseService: ExpenseService){ }
+  constructor(private expenseService: ExpenseService) { }
 
   ngOnInit(): void {
     this.getAllExpenses();
   }
 
-  getAllExpenses(){
+  getAllExpenses() {
     this.expenseService.getAllExpenses().snapshotChanges().subscribe({
-      next: (data)=> {
-        console.log('Data ==> ',data)
+      next: (data) => {
+        console.log('Data ==> ', data)
         // need to reset this as blank otherwise it will be pushed already
         this.expenses = [];
-        data.forEach( (item) => {
+        data.forEach((item) => {
           let expense = item.payload.toJSON() as IExpense
           console.log('Expense ==>', expense)
           // Converts a string to an integer or A string to convert into a number
           this.totalExpenses += parseInt(expense.price);
           // Since Key is optional I will add a empty string
-          // this.expenses
-        })
+          this.expenses.push({
+            key: item.key || '',
+            title: expense.title,
+            description: expense.description,
+            price: expense.price
+          });
+
+        });
+
       },
-    })
+    });
   }
 
 
